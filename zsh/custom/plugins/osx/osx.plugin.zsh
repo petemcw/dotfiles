5eda8a56f6ce119375cfbf6475fe3bbd7718e3ed
Pete McWilliams <pmcwilliams@augustash.com>
2012-08-02 12:27:36 -0500

    Stashing Redis config in the dots

--- zsh/custom/plugins/osx/osx.plugin.zsh
+++ zsh/custom/plugins/osx/osx.plugin.zsh
@@ -16,7 +16,7 @@ then
 fi
 
 # Redis
-alias redis-up='redis-server /usr/local/etc/redis.conf > /dev/null &'
+alias redis-up='redis-server $DOTS/redis/redis.conf > /dev/null &'
 alias redis-down='killall redis-server'
 
 # Fix Mac OS X delete key issue
