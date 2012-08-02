28f574caf8ba3b9550b6c183adc5945df0ad2dc4
Pete McWilliams <pmcwilliams@augustash.com>
2012-08-02 12:26:28 -0500

    Updated OS X defaults for Mountain Lion

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -59,3 +59,14 @@ defaults write com.apple.Mail DisableSendAnimations -bool true
 
 # Show the ~/Library folder
 chflags nohidden ~/Library
+
+# Set the Finder prefs for showing a few different volumes on the Desktop
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
+
+# Set up Safari for development
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
