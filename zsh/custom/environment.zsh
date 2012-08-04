1e425d08d97e94317f1e96dcb2a6f62788c261bf
Pete McWilliams <pmcwilliams@augustash.com>
2012-08-03 22:19:14 -0500

    Fixed missing rbenv PATH and theme

--- zsh/custom/environment.zsh
+++ zsh/custom/environment.zsh
@@ -1,4 +1,4 @@
-export PATH="$HOME/.bin:$HOME/.rbenv/shims:$HOME/.rvm/bin:/usr/local/bin:/usr/local/sbin:$PATH"
+export PATH="$HOME/.bin:$HOME/.rbenv/bin:$HOME/.rbenv/shims:$HOME/.rvm/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
 # editor
